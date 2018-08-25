7776b32f61454ca3bed1f27898d7b87572df098e
zhifenx <zhifenx@pupupula.com>
2018-08-25 11:51:11 +0800

    init git projrct

---
+++ Git/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git
+//
+//  Created by zhifenx on 2018/8/25.
+//  Copyright © 2018年 zhifenx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Git/ViewController.m
@@ -0,0 +1,31 @@
+//
+//  ViewController.m
+//  Git
+//
+//  Created by zhifenx on 2018/8/25.
+//  Copyright © 2018年 zhifenx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
